89446925ebc0a07ac048bf4c3ef0cf06ebabbd45
Yankee <yankee.exe@gmail.com>
2018-06-26 02:41:33 +0545

    Added Help with yargs

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const  request = require('request');
+const request = require('request');
+const yargs = require('yargs');
+const argv = yargs
+    .options({
+        a: {
+            demand: true, 
+            alias: 'address',
+            describe: 'Adress to fetch weather.',
+            string: true
+        }
+    })
+    .help()
+    .alias('help','h') //alias for help
+    .argv;
 
+console.log(argv);
 
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     //incoming data will be json take it and convert it to an object.
-    json: true 
-}, (error, response,body) => {
-    let dir = body.results[0].geometry.location;
+    json: true
+}, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${dir.lat}`);
-    console.log(`Longitude: ${dir.lng}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 })
