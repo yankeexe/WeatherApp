1759c8233328f2303f0edfcebe36ce7e10501426
Yankee <yankee.exe@gmail.com>
2018-06-27 11:57:29 +0545

    Redo app.js using promise | added default location

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const geocode = require('./geocode/geocode');
 const weather = require('./weather/weather');
 const yargs = require('yargs');
 
+
 const argv = yargs
     .options({
         a: {
             demand: true,
             alias: 'address',
             describe: 'Adress to fetch weather.',
+            string: true
+        },
+        u:{
+            alias: 'units',
+            describe: 'Desired unit of Weather Info, more on Readme.md',
             string: true
         }
     })
@@ -19,12 +25,12 @@ geocode.geocodeAddress(argv.a, (errorMessage, results) => {
     if (errorMessage) {
         console.log(errorMessage);
     } else {
-        console.log(results.address);
-        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+        console.log("Address:",results.address);
+        weather.getWeather(results.latitude, results.longitude, argv.u, (errorMessage, weatherResults) => {
             if (errorMessage) {
                 console.log(errorMessage);
             } else {
-                console.log(`It is currently ${weatherResults.summary}. And the temperature is ${weatherResults.temperature}.`)
+                console.log(`Weather Forecast: It is currently ${weatherResults.summary}. And the temperature is ${weatherResults.temperature} degree celcius. The Cloud Cover is ${weatherResults.cloudCover}`)
             }
         });
     }
--- weather/weather.js
+++ weather/weather.js
@@ -1,10 +1,13 @@
 const request = require('request');
 
-let getWeather = (la, lo, callback) => {
+let getWeather = (la, lo,units, callback) => {
+   /**check if the value of un is true 
+    * and if it is then place it on the URL else don't */
+   var un = units ? `?units=${units}` : "";
     request({
-        url: `https://api.darksky.net/forecast/8b56fc292a653b45f75e0291085630d0/${la},${lo}`,
+        url: `https://api.darksky.net/forecast/8b56fc292a653b45f75e0291085630d0/${la},${lo}${un}`,
         json: true
-    }, (error, resonse, body) => {
+    },(error, resonse, body) => {
         if (error) {
             callback("Unable to connect to the server");
         } else if (body.error === "The given location (or time) is invalid.") {
@@ -12,7 +15,9 @@ let getWeather = (la, lo, callback) => {
         } else {
             callback(undefined, {
                 summary: body.currently.summary,
-                temperature: body.currently.temperature
+                temperature: body.currently.temperature,
+                cloudCover: body.currently.cloudCover
+
             })
 
         }
